461728fe57f41cfdbe9ea138e6e231519972e931
lyndonpett <lyndonpett98@gmail.com>
2021-09-01 13:56:21 -0500

    Fixed free

--- shell.c
+++ shell.c
@@ -41,7 +41,8 @@ int main(int ac, char **av)
 		xcuteable = dir(argv, path_tokens, av, counter);
 		if (xcuteable != NULL)
 			execute(xcuteable, argv);
-		free(xcuteable);
+		if (argv[0][0] != '/')
+			free(xcuteable);
 		everything_free(path_tokens);
 		free(argv);
 	}
